befb03458eda139321a34b768b82b9a5b50553b9
Danila Zhukov <danila@Mac.fritz.box>
2026-03-27 15:04:43 +0100

    add solution to ex1

---
+++ 00/ex1/ft_garden_name.py
@@ -0,0 +1,16 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_garden_name.py                                  :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: dzhukov <dzhukov@student.42heilbronn.de    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/03/27 14:55:48 by dzhukov           #+#    #+#              #
+#    Updated: 2026/03/27 15:02:37 by dzhukov          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+def ft_garden_name():
+    string = input("Enter garden name: ")
+    print("Garden:", string)
+    print("Status: Growing well!")
